1f793b22f07ac319ab288ffac53df5c08c7836ef
rajeshtanne <rajeshtanne@ntar.com>
2025-12-03 13:18:06 +0530

    fix:testing  in industry/category/brand, fixed brand update logic, and removed unused brand delete functions

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+// import { Provider } from "react-redux";
+// import { PersistGate } from "redux-persist/integration/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import store, { persistor } from "./redux/store.js";
+// import store, { persistor } from "./redux/store.js";
 import "./index.css";
 import queryClient from "./react-query/queryClient.js";
 
@@ -20,13 +20,13 @@ if ("serviceWorker" in navigator) {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <QueryClientProvider client={queryClient}>
-        <PersistGate loading={null} persistor={persistor}>
+        {/* <PersistGate loading={null} persistor={persistor}> */}
           <App />
           <ReactQueryDevtools initialIsOpen={false} />
-        </PersistGate>
+        {/* </PersistGate> */}
       </QueryClientProvider>
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
