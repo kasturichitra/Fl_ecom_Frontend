2ca5dbcb735133dc1330ebad504c2832d2686975
Bhanu Prasad Suram <BhanuPrasad@ntar.local>
2025-12-04 15:32:22 +0530

    ui update

--- src/pages/Orders/OrderListManager.jsx
+++ src/pages/Orders/OrderListManager.jsx
@@ -151,7 +151,7 @@ const OrderListManager = () => {
   });
 
   return (
-    <div className="min-h-screen bg-gray-50 py-4 px-4">
+    <div className="min-h-screen bg-gray-50 py-10">
       <div className="bg-white shadow-2xl rounded-2xl overflow-hidden border border-gray-200">
         {/* HEADER */}
         <div className="bg-gradient-to-r from-indigo-600 to-purple-700 text-white px-2 py-2">
