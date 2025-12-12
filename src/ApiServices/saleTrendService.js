d201573b5adc72291e6f3fb45efb639a8a65ec17
Sandeep Kumar Lingamguntla <SandeepKumar@ntar.local>
2025-12-12 10:34:05 +0530

    CRUD APIs for sale trends

--- src/ApiServices/saleTrendService.js
+++ src/ApiServices/saleTrendService.js
@@ -6,3 +6,9 @@ export const getSaleTrends = (params = {}) => {
   const queryString = new URLSearchParams(params).toString();
   return axiosInstance.get(`${BASE_URL}${queryString ? `?${queryString}` : ""}`);
 };
+
+export const createSaleTrend = (data) => axiosInstance.post(`${BASE_URL}`, data);
+
+export const updateSaleTrend = (id, data) => axiosInstance.put(`${BASE_URL}/${id}`, data);
+
+export const deleteSaleTrend = (id) => axiosInstance.delete(`${BASE_URL}/${id}`);
--- src/hooks/useSaleTrend.js
+++ src/hooks/useSaleTrend.js
@@ -1,15 +1,58 @@
-import { useQuery } from "@tanstack/react-query";
-import { getSaleTrends } from "../ApiServices/saleTrendService";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createSaleTrend, deleteSaleTrend, getSaleTrends, updateSaleTrend } from "../ApiServices/saleTrendService";
+import toast from "react-hot-toast";
 
 export const useGetAllSaleTrends = ({ searchTerm = "", page = 1, limit = 10, sort = "" }) => {
-  const queryKey = ["brands", searchTerm || "", page, limit, sort];
+  const queryKey = ["saleTrends", searchTerm || "", page, limit, sort];
 
   return useQuery({
     queryKey,
     queryFn: () => getSaleTrends({ searchTerm, page, limit, sort }),
-    select: (res) => res?.data, 
+    select: (res) => res?.data,
     staleTime: 10 * 60 * 1000,
     cacheTime: 30 * 60 * 1000,
     refetchOnMount: false,
   });
 };
+
+export const useCreateSaleTrend = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data) => createSaleTrend(data),
+    onSuccess: () => {
+      toast.success("Sale Trend created successfully");
+      queryClient.invalidateQueries({ queryKey: ["saleTrends"] });
+    },
+    onError: () => {
+      toast.error("Failed to create brand");
+    },
+  });
+};
+
+export const useUpdateSaleTrend = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ id, data }) => updateSaleTrend(id, data),
+    onSuccess: () => {
+      toast.success("Sale Trend updated successfully");
+      queryClient.invalidateQueries({ queryKey: ["saleTrends"] });
+    },
+    onError: () => {
+      toast.error("Failed to update Sale Trend");
+    },
+  });
+};
+
+export const useDeleteSaleTrend = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (id) => deleteSaleTrend(id),
+    onSuccess: () => {
+      toast.success("Sale Trend deleted successfully");
+      queryClient.invalidateQueries({ queryKey: ["saleTrends"] });
+    },
+    onError: () => {
+      toast.error("Failed to delete Sale Trend");
+    },
+  });
+}
