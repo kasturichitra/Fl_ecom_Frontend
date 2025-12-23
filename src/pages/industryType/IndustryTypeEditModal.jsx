639fd207211d163154d1283536a606d47a48bf8d
Sandeep Kumar Lingamguntla <SandeepKumar@ntar.local>
2025-12-23 10:40:46 +0530

    Update Industry integrated with images

--- src/pages/industryType/IndustryTypeEditModal.jsx
+++ src/pages/industryType/IndustryTypeEditModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import EditModalLayout from "../../components/EditModalLayout";
 import DynamicForm from "../../components/DynamicForm";
+import toBase64 from "../../utils/toBase64";
 
 const IndustryTypeEditModal = ({ formData: initialData, closeModal, onSubmit, isSubmitting }) => {
   const [formData, setLocalFormData] = useState({
@@ -12,19 +13,28 @@ const IndustryTypeEditModal = ({ formData: initialData, closeModal, onSubmit, is
   // Submit handler
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const submitData = new FormData();
+    // const submitData = new FormData();
 
-    submitData.append("industry_name", formData?.industry_name || "");
-    submitData.append("industry_unique_id", formData?.industry_unique_id || "");
-    submitData.append("description", formData?.description || "");
-    submitData.append("is_active", formData?.is_active ?? true);
+    // submitData.append("industry_name", formData?.industry_name || "");
+    // submitData.append("industry_unique_id", formData?.industry_unique_id || "");
+    // submitData.append("description", formData?.description || "");
+    // submitData.append("is_active", formData?.is_active ?? true);
 
     // Only append NEW image
-    if (imageFile) {
-      submitData.append("image", imageFile);
+    // if (imageFile) {
+    //   submitData.append("image", imageFile);
+    // }
+
+    const imageBase64 = await toBase64(formData?.image);
+    const payload = {
+      industry_name: formData?.industry_name,
+      industry_unique_id: formData?.industry_unique_id,
+      description: formData?.description,
+      is_active: formData?.is_active,
+      image_base64: imageBase64,
     }
 
-    await onSubmit(submitData);
+    await onSubmit(payload);
   };
 
   const fields = [
