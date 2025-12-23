fc80b5ae5aa7c908083c33862efdbe57d97aa532
rajeshtanne <rajeshtanne@ntar.com>
2025-12-23 11:12:53 +0530

    checking

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://10.1.1.156:3000",
+        target: "http://10.1.1.130:3000",
         // target: "http://10.1.1.120:3000",
         changeOrigin: true,
         secure: false,
